a9a13d433105927e06b3e1bcd8dbf94cf7d9e5fe
Grelet Sacha <sacha.grelet@telecom-st-etienne.fr>
2018-05-24 11:36:28 +0200

    [ADD] PlayerConstructor

--- player.cpp
+++ player.cpp
@@ -1,6 +1,13 @@
 #include "player.h"
 
-Player::Player(String name)
+Player::Player(String name, int pv, int score, bool shieldEnable, int shieldCD, QImage photo)
 {
     name_ = name;
+    pv_ = pv;
+    score_ = score;
+    shieldEnable_ = shieldEnable;
+    shieldCD_ = shieldCD;
+    photo_ = photo;
 }
+
+
