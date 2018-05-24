30efd96d5d393c6d5983d392db367c40151cfd17
Grelet Sacha <sacha.grelet@telecom-st-etienne.fr>
2018-05-24 12:13:03 +0200

    [ADD] Attack Constructor & damage

--- player.h
+++ player.h
@@ -1,11 +1,11 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include <QImage>
+
 #include "opencv2/video/tracking.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
-
-#include <QImage>
 
 using namespace std;
 using namespace cv;
